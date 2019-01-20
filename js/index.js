db3a4f36d0e266d57032cb55912dcc2f65061fef
Dale Shlass <dshlass@gmail.com>
2019-01-20 12:23:33 -0500

    javascript separated

---
+++ js/index.js
@@ -0,0 +1,46 @@
+$(function() {
+    $('a').smoothScroll({
+        offset: -98
+    });
+});
+
+navWrapper = document.getElementById("navWrapper");
+stickyWrapper= document.getElementById("sticky-wrapper");
+scrollToTop = document.getElementById("scrollToTop"); 
+
+
+function getYPosition() {
+  return window.scrollY;
+}
+
+function isChecked() {
+  return checked;
+}
+
+function navSticky() {
+  if (getYPosition() >= 1 ) {
+    navWrapper.className = "nav-wrapper nav-fixed";
+  } else {navWrapper.className = "nav-wrapper";
+}
+};
+
+function navShadow() {
+  if (getYPosition() >= 1 ) {
+        stickyWrapper.className = "sticky-wrapper drop-down-shadow";
+  } else { stickyWrapper.className = "sticky-wrapper";
+  }
+}
+
+function toTop() {
+  if (getYPosition() >= 300 ) {
+    scrollToTop.className = "back-to-top back-to-top-show";
+  } else {
+    scrollToTop.className = "back-to-top";
+  }
+};
+
+// injectStyles('a:hover { color: red; }'); FOR USE LATER ON IF NEEDED
+
+window.addEventListener("scroll", navSticky);
+window.addEventListener("scroll", navShadow);
+window.addEventListener("scroll", toTop);
