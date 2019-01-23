29a1281fb2f8799ae687b9a59d9dce668e19077c
Dale Shlass <dshlass@gmail.com>
2019-01-22 22:46:53 -0500

    Updated files from live site

--- js/index.js
+++ js/index.js
@@ -1,29 +1,30 @@
 $(function() {
     
-    //smoothScroll functionality
-    $('a').smoothScroll({
-        offset: -98
-    });
+  //smoothScroll functionality
+  $('a').smoothScroll({
+      offset: -98,
+      speed: 1000
+  });
 
-    //For the drop-down menu
-    $('a').click(function(){
-      if ($(window).width() <= 961) {
-        $('#toggle').prop('checked', false);
-      }
-    });
+  //For the drop-down menu
+  $('a').click(function(){
+    if ($(window).width() <= 961) {
+      $('#toggle').prop('checked', false);
+    }
+  });
 
-    //For the Resume page
-    $('#experience').click(function(){
-      $('#resume').prop('checked', true);
-      $('#experience').addClass('no-animation');
-      $('#education').removeClass('no-animation');
-    });
-    
-    $('#education').click(function(){
-      $('#resume').prop('checked', false);
-      $('#education').addClass('no-animation');
-      $('#experience').removeClass('no-animation');
-    });
+  //For the Resume page
+  $('#experience').click(function(){
+    $('#resume').prop('checked', true);
+    $('#experience').addClass('no-animation');
+    $('#education').removeClass('no-animation');
+  });
+  
+  $('#education').click(function(){
+    $('#resume').prop('checked', false);
+    $('#education').addClass('no-animation');
+    $('#experience').removeClass('no-animation');
+  });
 });
 
 //Section to control the navigation menu
@@ -33,33 +34,33 @@ scrollToTop = document.getElementById("scrollToTop");
 
 
 function getYPosition() {
-  return window.scrollY;
+return window.scrollY;
 }
 
 function isChecked() {
-  return checked;
+return checked;
 }
 
 function navSticky() {
-  if (getYPosition() >= 1 ) {
-    navWrapper.className = "nav-wrapper nav-fixed";
-  } else {navWrapper.className = "nav-wrapper";
+if (getYPosition() >= 1 ) {
+  navWrapper.className = "nav-wrapper nav-fixed";
+} else {navWrapper.className = "nav-wrapper";
 }
 };
 
 function navShadow() {
-  if (getYPosition() >= 1 ) {
-        stickyWrapper.className = "sticky-wrapper drop-down-shadow";
-  } else { stickyWrapper.className = "sticky-wrapper";
-  }
+if (getYPosition() >= 1 ) {
+      stickyWrapper.className = "sticky-wrapper drop-down-shadow";
+} else { stickyWrapper.className = "sticky-wrapper";
+}
 }
 
 function toTop() {
-  if (getYPosition() >= 300 ) {
-    scrollToTop.className = "back-to-top back-to-top-show";
-  } else {
-    scrollToTop.className = "back-to-top";
-  }
+if (getYPosition() >= 300 ) {
+  scrollToTop.className = "back-to-top back-to-top-show";
+} else {
+  scrollToTop.className = "back-to-top";
+}
 };
 
 window.addEventListener("scroll", navSticky);
